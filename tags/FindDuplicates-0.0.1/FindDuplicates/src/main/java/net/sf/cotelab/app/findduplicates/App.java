367ede240609bef631cde526635d7dab50361817
rfc11fan <rfc11fan@5d029a7c-2d57-4ffb-80e6-f4efc12199a5>
2014-09-11 14:54:20 +0000

    Tag the release version.

---
+++ tags/FindDuplicates-0.0.1/FindDuplicates/src/main/java/net/sf/cotelab/app/findduplicates/App.java
@@ -0,0 +1,13 @@
+package net.sf.cotelab.app.findduplicates;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
