d1f1e2bbaaa880a37e89f05efefee84eb1103742
rfc11fan <rfc11fan@5d029a7c-2d57-4ffb-80e6-f4efc12199a5>
2015-02-03 18:15:08 +0000

    Limit the number of places where decks are populated.

--- trunk/LaBelleLucie/src/main/java/net/sf/cotelab/lbl/model/impl/Pack.java
+++ trunk/LaBelleLucie/src/main/java/net/sf/cotelab/lbl/model/impl/Pack.java
@@ -1,47 +1,12 @@
 package net.sf.cotelab.lbl.model.impl;
 
-import net.sf.cotelab.playingcards.Card;
-import net.sf.cotelab.playingcards.Deck;
-import net.sf.cotelab.playingcards.Rank;
-import net.sf.cotelab.playingcards.Suit;
+import net.sf.cotelab.playingcards.decks.BridgeDeck;
 
 /**
  * A pack of cards suitable for playing La Belle Lucie.
  */
-public class Pack extends Deck {
-	public static final Rank[] RANKS = {
-		Rank.ACE,
-		Rank.DEUCE,
-		Rank.TREY,
-		Rank.FOUR,
-		Rank.FIVE,
-		Rank.SIX,
-		Rank.SEVEN,
-		Rank.EIGHT,
-		Rank.NINE,
-		Rank.TEN,
-		Rank.JACK,
-		Rank.QUEEN,
-		Rank.KING
-	};
-	public static final Suit[] SUITS = {
-		Suit.CLUB,
-		Suit.DIAMOND,
-		Suit.HEART,
-		Suit.SPADE
-	};
-
+public class Pack extends BridgeDeck {
 	public Pack() {
 		super();
-
-		for (Suit suit : SUITS) {
-			for (Rank rank : RANKS) {
-				add(newCard(rank, suit));
-			}
-		}
-	}
-
-	protected Card newCard(Rank rank, Suit suit) {
-		return new Card(rank, suit);
 	}
 }
