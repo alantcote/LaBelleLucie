484c5eff0277a690f1b03711667144bb1c09d369
rfc11fan <96086810+alantcote@users.noreply.github.com>
2015-02-03 16:53:20 +0000

    Polishing this apple.

--- CLPlayingCards/src/main/java/net/sf/cotelab/playingcards/demo/DeckViewerScene.java
+++ CLPlayingCards/src/main/java/net/sf/cotelab/playingcards/demo/DeckViewerScene.java
@@ -3,7 +3,15 @@ package net.sf.cotelab.playingcards.demo;
 import javafx.scene.Scene;
 import javafx.scene.paint.Paint;
 
+/**
+ * The scene for the deck viewer.
+ * @author cote
+ */
 public class DeckViewerScene extends Scene {
+	/**
+	 * Construct a new object, using a specified background color.
+	 * @param backgroundColor the background color.
+	 */
 	public DeckViewerScene(Paint backgroundColor) {
 		super(new DeckViewPane(), backgroundColor);
 	}
