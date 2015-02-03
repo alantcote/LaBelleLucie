d22ec7da1f27b1d12cc124c52b6ab1b286a58e24
rfc11fan <rfc11fan@5d029a7c-2d57-4ffb-80e6-f4efc12199a5>
2015-02-03 16:53:20 +0000

    Polishing this apple.

--- trunk/CLPlayingCards/src/main/java/net/sf/cotelab/playingcards/demo/DeckViewer.java
+++ trunk/CLPlayingCards/src/main/java/net/sf/cotelab/playingcards/demo/DeckViewer.java
@@ -6,13 +6,24 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
+/**
+ * An application to demonstrate the playing cards, by displaying their images.
+ * @author cote
+ */
 public class DeckViewer extends Application {
 	public static final Paint BACKGROUND_COLOR = Color.DARKGREEN;
 	
+	/**
+	 * The standard entry-point for a Java application.
+	 * @param args the command-line arguments.
+	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
 	
+	/**
+	 * The standard entry-point for JavaFX applications.
+	 */
 	@Override
 	public void start(final Stage primaryStage) {
 		Scene scene;
@@ -27,6 +38,10 @@ public class DeckViewer extends Application {
 		primaryStage.show();
 	}
 
+	/**
+	 * Create a new scene.
+	 * @return the new scene.
+	 */
 	protected Scene newScene() {
 		return new DeckViewerScene(BACKGROUND_COLOR);
 	}
