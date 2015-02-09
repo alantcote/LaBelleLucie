f66a0a7bd520e5b3d464c02d2a551aa66d8bacaa
rfc11fan <96086810+alantcote@users.noreply.github.com>
2015-02-09 16:11:47 +0000

    Polishing the apple.

--- LaBelleLucie/src/main/java/net/sf/cotelab/lbl/controller/impl/undoableop/MoveCardTableauToTableauOp.java
+++ LaBelleLucie/src/main/java/net/sf/cotelab/lbl/controller/impl/undoableop/MoveCardTableauToTableauOp.java
@@ -4,11 +4,22 @@ import net.sf.cotelab.lbl.controller.impl.ControllerImpl;
 import net.sf.cotelab.lbl.model.facade.GameSummary;
 import net.sf.cotelab.lbl.undo.UndoableOp;
 
+/**
+ * An <tt>UndoableOp</tt> that implements playing a the top card from one
+ * tableau fan to the top of another tableau fan.
+ * @author cote
+ */
 public class MoveCardTableauToTableauOp implements UndoableOp {
 	protected ControllerImpl controller;
 	protected int destFanIndex;
 	protected int srcFanIndex;
 
+	/**
+	 * Construct a new object.
+	 * @param controller the controller.
+	 * @param srcFanIndex the index of the source tableau fan.
+	 * @param destFanIndex the index of the destination tableau fan.
+	 */
 	public MoveCardTableauToTableauOp(
 			ControllerImpl controller, int srcFanIndex, int destFanIndex) {
 		super();
---
+++ LaBelleLucie/src/test/java/net/sf/cotelab/lbl/controller/impl/undoableop/MoveCardTableauToTableauOpTest.java
@@ -0,0 +1,80 @@
+package net.sf.cotelab.lbl.controller.impl.undoableop;
+
+import static org.junit.Assert.*;
+import javafx.beans.property.ObjectProperty;
+import net.sf.cotelab.lbl.controller.impl.ControllerImpl;
+import net.sf.cotelab.lbl.model.facade.GameState;
+import net.sf.cotelab.lbl.model.facade.GameSummary;
+import net.sf.cotelab.testutils.jMockTestHelper;
+
+import org.jmock.Expectations;
+import org.junit.Test;
+
+public class MoveCardTableauToTableauOpTest extends jMockTestHelper {
+	@Test
+	public void testDoOp() {
+		final ControllerImpl mockControllerImpl =
+				context.mock(ControllerImpl.class, "mockControllerImpl");
+		final int srcFanIndex = 0;
+		final int destFanIndex = 0;
+		MoveCardTableauToTableauOp fixture =
+				new MoveCardTableauToTableauOp(
+						mockControllerImpl, srcFanIndex, destFanIndex);
+		
+		context.checking(new Expectations() {{
+			oneOf(mockControllerImpl).moveTopCardTableauToTableau(
+					srcFanIndex, destFanIndex);
+
+			oneOf(mockControllerImpl).updateGameSummary();
+		}});
+		
+		fixture.doOp();
+	}
+
+	@Test
+	public void testMoveCardTableauToTableauOp() {
+		final ControllerImpl mockControllerImpl =
+				context.mock(ControllerImpl.class, "mockControllerImpl");
+		final int srcFanIndex = 0;
+		final int destFanIndex = 0;
+		MoveCardTableauToTableauOp fixture =
+				new MoveCardTableauToTableauOp(
+						mockControllerImpl, srcFanIndex, destFanIndex);
+		
+		assertEquals(mockControllerImpl, fixture.controller);
+		assertEquals(srcFanIndex, fixture.srcFanIndex);
+		assertEquals(destFanIndex, fixture.destFanIndex);
+	}
+
+	@Test
+	public void testUndoOp() {
+		final ControllerImpl mockControllerImpl =
+				context.mock(ControllerImpl.class, "mockControllerImpl");
+		final int srcFanIndex = 0;
+		final int destFanIndex = 0;
+		MoveCardTableauToTableauOp fixture =
+				new MoveCardTableauToTableauOp(
+						mockControllerImpl, srcFanIndex, destFanIndex);
+		final GameState mockGameState =
+				context.mock(GameState.class, "mockGameState");
+		@SuppressWarnings("unchecked")
+		final ObjectProperty<GameSummary> mockGameSummary =
+				(ObjectProperty<GameSummary>)
+				context.mock(ObjectProperty.class, "mockGameSummary");
+		
+		context.checking(new Expectations() {{
+			oneOf(mockControllerImpl).moveTopCardTableauToTableau(
+					destFanIndex, srcFanIndex);
+
+			oneOf(mockControllerImpl).getModel();
+			will(returnValue(mockGameState));
+			
+			oneOf(mockGameState).getGameSummary();
+			will(returnValue(mockGameSummary));
+			
+			oneOf(mockGameSummary).set(GameSummary.IN_PROGRESS);
+		}});
+		
+		fixture.undoOp();
+	}
+}
