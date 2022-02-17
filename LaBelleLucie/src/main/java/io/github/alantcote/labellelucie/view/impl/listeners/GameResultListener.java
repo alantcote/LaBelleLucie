b3f3ed608724cb86e64f5e627f6161389e74ffa8
Alan T. Coté <96086810+alantcote@users.noreply.github.com>
2022-02-17 13:55:56 -0500

    Add a test case.

--- LaBelleLucie/src/main/java/io/github/alantcote/labellelucie/view/impl/listeners/GameResultListener.java
+++ LaBelleLucie/src/main/java/io/github/alantcote/labellelucie/view/impl/listeners/GameResultListener.java
@@ -11,6 +11,17 @@ import javafx.scene.control.ButtonType;
 import javafx.stage.Window;
 
 public class GameResultListener implements ChangeListener<GameSummary> {
+	public static final Alert.AlertType ALERT_TYPE = Alert.AlertType.INFORMATION;
+	public static final String ALL_CARDS_PLAYED = "All cards have been moved to foundations.";
+	public static final String EXIT_PROGRAM = "Exit";
+	public static final ButtonType EXIT_PROGRAM_BT = new ButtonType(EXIT_PROGRAM);
+	public static final String GAME_LOST = "Game lost";
+	public static final String GAME_WON = "Game won";
+	public static final String NEW_GAME = "Start new game";
+	public static final ButtonType NEW_GAME_BT = new ButtonType(NEW_GAME);
+	public static final String NO_LEGAL_PLAYS = "There are no legal plays remaining.";
+	public static final String TRY_AGAIN = "Return and try again";
+	public static final ButtonType TRY_AGAIN_BT = new ButtonType(TRY_AGAIN);
 	protected InputHandler inputHandler = null;
 	protected Window window;
 
@@ -22,44 +33,7 @@ public class GameResultListener implements ChangeListener<GameSummary> {
 
 	@Override
 	public void changed(ObservableValue<? extends GameSummary> src, GameSummary oldValue, GameSummary newValue) {
-		Alert.AlertType alertType = Alert.AlertType.INFORMATION;
-		Alert alert = new Alert(alertType);
-		Optional<ButtonType> result;
-		String tryAgain = "Return and try again";
-		ButtonType tryAgainBT = new ButtonType(tryAgain);
-		String newGame = "Start new game";
-		ButtonType newGameBT = new ButtonType(newGame);
-		String exitProgram = "Exit";
-		ButtonType exitProgramBT = new ButtonType(exitProgram);
-
-		switch (newValue) {
-		case LOST:
-			alert = new Alert(alertType, "There are no legal plays remaining.", tryAgainBT, newGameBT, exitProgramBT);
-			alert.setHeaderText("Game lost");
-
-			break;
-		case WON:
-			alert = new Alert(alertType, "All cards have been moved to foundations.", newGameBT, exitProgramBT);
-			alert.setHeaderText("Game won");
-			
-			break;
-		default:
-			return;
-		}
-		
-		result = alert.showAndWait();
-		
-		if (result.isPresent()) {
-			ButtonType chosen = result.get();
-
-			if (newGameBT == chosen) {
-				inputHandler.onNewGameRequested();
-			}
-
-			if (exitProgramBT == chosen) {
-				inputHandler.onExitRequest();
-			}
-		}
+		processEvent(newValue);
 	}
 
 	/**
@@ -67,5 +41,84 @@ public class GameResultListener implements ChangeListener<GameSummary> {
 	 */
 	public void setInputHandler(InputHandler inputHandler) {
 		this.inputHandler = inputHandler;
+	}
+
+	/**
+	 * @return
+	 */
+	protected Alert newGameLostAlert() {
+		Alert alert = new Alert(ALERT_TYPE, NO_LEGAL_PLAYS, TRY_AGAIN_BT, NEW_GAME_BT, EXIT_PROGRAM_BT);
+
+		alert.setHeaderText(GAME_LOST);
+
+		return alert;
+	}
+
+	/**
+	 * @return
+	 */
+	protected Alert newGameWonAlert() {
+		Alert alert = new Alert(ALERT_TYPE, ALL_CARDS_PLAYED, NEW_GAME_BT, EXIT_PROGRAM_BT);
+
+		alert.setHeaderText(GAME_WON);
+
+		return alert;
+	}
+
+	/**
+	 * @param newValue
+	 */
+	protected void processEvent(GameSummary newValue) {
+		Alert alert;
+		Optional<ButtonType> result;
+
+		switch (newValue) {
+		case LOST:
+			alert = newGameLostAlert();
+
+			break;
+		case WON:
+			alert = newGameWonAlert();
+
+			break;
+		default:
+			return;
+		}
+
+		result = showAndWait(alert);
+
+		if (result.isPresent()) {
+			ButtonType chosen = result.get();
+
+			if (NEW_GAME_BT == chosen) {
+				requestNewGame();
+			}
+
+			if (EXIT_PROGRAM_BT == chosen) {
+				requestProgramExit();
+			}
+		}
+	}
+
+	/**
+	 * 
+	 */
+	protected void requestNewGame() {
+		inputHandler.onNewGameRequested();
+	}
+
+	/**
+	 * 
+	 */
+	protected void requestProgramExit() {
+		inputHandler.onExitRequest();
+	}
+
+	/**
+	 * @param alert
+	 * @return
+	 */
+	protected Optional<ButtonType> showAndWait(Alert alert) {
+		return alert.showAndWait();
 	}
 }
---
+++ LaBelleLucie/src/test/java/io/github/alantcote/labellelucie/view/impl/listeners/GameResultListenerTest.java
@@ -0,0 +1,198 @@
+package io.github.alantcote.labellelucie.view.impl.listeners;
+
+import static org.junit.Assert.*;
+
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import de.saxsys.mvvmfx.testingutils.jfxrunner.JfxRunner;
+import de.saxsys.mvvmfx.testingutils.jfxrunner.TestInJfxThread;
+import io.github.alantcote.labellelucie.controller.facade.InputHandler;
+import io.github.alantcote.labellelucie.controller.impl.ControllerImpl;
+import io.github.alantcote.labellelucie.controller.impl.handler.MasterInputHandler;
+import io.github.alantcote.labellelucie.model.facade.GameState;
+import io.github.alantcote.labellelucie.model.facade.GameSummary;
+import io.github.alantcote.labellelucie.model.impl.GameStateImpl;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+
+/**
+ * Test method for {@link io.github.alantcote.labellelucie.view.impl.listeners.GameResultListener}.
+ */
+@RunWith(JfxRunner.class)
+public class GameResultListenerTest {
+
+	/**
+	 * Test method for {@link io.github.alantcote.labellelucie.view.impl.listeners.GameResultListener#GameResultListener(javafx.stage.Window)}.
+	 */
+	@Test
+	@TestInJfxThread
+	public void testGameResultListener() {
+		Stage testStage = new Stage();
+		GameResultListener fixture = new GameResultListener(testStage);
+		
+		assertTrue(testStage == fixture.window);
+	}
+
+	/**
+	 * Test method for {@link io.github.alantcote.labellelucie.view.impl.listeners.GameResultListener#changed(javafx.beans.value.ObservableValue, io.github.alantcote.labellelucie.model.facade.GameSummary, io.github.alantcote.labellelucie.model.facade.GameSummary)}.
+	 */
+	@Test
+	@TestInJfxThread
+	public void testChanged() {
+		SimpleIntegerProperty testState = new SimpleIntegerProperty(0);
+		Stage testStage = new Stage();
+		GameResultListener fixture = new GameResultListener(testStage) {
+
+			@Override
+			protected void processEvent(GameSummary newValue) {
+				assertTrue(0 == testState.get());
+				
+				testState.set(1);
+				
+				assertTrue(GameSummary.LOST == newValue);
+			}
+			
+		};
+		GameSummary testGameSummary = GameSummary.LOST;
+		
+		fixture.changed(null, GameSummary.IN_PROGRESS, GameSummary.LOST);
+		
+		assertTrue(1 == testState.get());
+	}
+
+	/**
+	 * Test method for {@link io.github.alantcote.labellelucie.view.impl.listeners.GameResultListener#setInputHandler(io.github.alantcote.labellelucie.controller.facade.InputHandler)}.
+	 */
+	@Test
+	@TestInJfxThread
+	public void testSetInputHandler() {
+		Stage testStage = new Stage();
+		GameResultListener fixture = new GameResultListener(testStage);
+		GameState testGameState = new GameStateImpl();
+		ControllerImpl testControllerImpl = new ControllerImpl(testGameState);
+		InputHandler testInputHandler = new MasterInputHandler(testControllerImpl);
+		
+		assertTrue(null == fixture.inputHandler);
+		
+		fixture.setInputHandler(testInputHandler);
+		
+		assertTrue(testInputHandler == fixture.inputHandler);
+	}
+
+	/**
+	 * Test method for {@link io.github.alantcote.labellelucie.view.impl.listeners.GameResultListener#newGameLostAlert()}.
+	 */
+	@Test
+	@TestInJfxThread
+	public void testNewGameLostAlert() {
+		Stage testStage = new Stage();
+		GameResultListener fixture = new GameResultListener(testStage);
+		
+		assertNotNull(fixture.newGameLostAlert());
+	}
+
+	/**
+	 * Test method for {@link io.github.alantcote.labellelucie.view.impl.listeners.GameResultListener#newGameWonAlert()}.
+	 */
+	@Test
+	@TestInJfxThread
+	public void testNewGameWonAlert() {
+		Stage testStage = new Stage();
+		GameResultListener fixture = new GameResultListener(testStage);
+		
+		assertNotNull(fixture.newGameWonAlert());
+	}
+
+	/**
+	 * Test method for {@link io.github.alantcote.labellelucie.view.impl.listeners.GameResultListener#processEvent(io.github.alantcote.labellelucie.model.facade.GameSummary)}.
+	 */
+	@Test
+	@TestInJfxThread
+	public void testProcessEvent() {
+		SimpleIntegerProperty testState = new SimpleIntegerProperty(0);
+		SimpleObjectProperty<Alert> alertProperty = new SimpleObjectProperty<Alert>();
+		Stage testStage = new Stage();
+		GameResultListener fixture = new GameResultListener(testStage) {
+
+			@Override
+			protected Alert newGameLostAlert() {
+				assertTrue(0 == testState.get());
+				
+				testState.set(1);
+				
+				alertProperty.set(super.newGameLostAlert());
+				
+				return alertProperty.get();
+			}
+
+			@Override
+			protected Alert newGameWonAlert() {
+				fail("this method should not be called");
+				
+				return null;
+			}
+
+			@Override
+			protected void requestNewGame() {
+				assertTrue(2 == testState.get());
+				
+				testState.set(3);
+			}
+
+			@Override
+			protected void requestProgramExit() {
+				fail("this method should not be called");
+			}
+
+			@Override
+			protected Optional<ButtonType> showAndWait(Alert alert) {
+				assertTrue(1 == testState.get());
+				
+				testState.set(2);
+				
+				assertTrue(alertProperty.get() == alert);
+				
+				Optional<ButtonType> result = Optional.<ButtonType>ofNullable(GameResultListener.NEW_GAME_BT);
+				
+				return result;
+			}
+			
+		};
+		GameSummary testGameSummary = GameSummary.LOST;
+		
+		fixture.processEvent(GameSummary.LOST);
+		
+		assertTrue(3 == testState.get());
+	}
+
+	/**
+	 * Test method for {@link io.github.alantcote.labellelucie.view.impl.listeners.GameResultListener#requestNewGame()}.
+	 */
+	@Test
+	public void testRequestNewGame() {
+		// A one-liner. Stipulate that it works.
+	}
+
+	/**
+	 * Test method for {@link io.github.alantcote.labellelucie.view.impl.listeners.GameResultListener#requestProgramExit()}.
+	 */
+	@Test
+	public void testRequestProgramExit() {
+		// A one-liner. Stipulate that it works.
+	}
+
+	/**
+	 * Test method for {@link io.github.alantcote.labellelucie.view.impl.listeners.GameResultListener#showAndWait(javafx.scene.control.Alert)}.
+	 */
+	@Test
+	public void testShowAndWait() {
+		// Would open an Alert on the GUI. Not good in a unit test.
+	}
+
+}
